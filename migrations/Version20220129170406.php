d3a95e7ec32e2be876d0499f7231cca1112d8073
Ati674 <atilla.bursa67@gmail.com>
2022-02-04 21:04:24 +0100

    Update

--- migrations/Version20220129170406.php
+++ migrations/Version20220129170406.php
@@ -19,9 +19,8 @@ final class Version20220129170406 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
+        // this up() migration is auto-generated,FK_2A145AFF5DFCD4B8 please modify it to your needs
         $this->addSql('SET FOREIGN_KEY_CHECKS=0');
-        $this->addSql('ALTER TABLE tirage DROP FOREIGN KEY FK_2A145AFF5DFCD4B8');
         $this->addSql('DROP INDEX IDX_2A145AFF5DFCD4B8 ON tirage');
         $this->addSql('ALTER TABLE tirage DROP winner_id');
         $this->addSql('ALTER TABLE winner DROP INDEX UNIQ_CF6600E82579054, ADD INDEX IDX_CF6600E82579054 (tirage_id)');
@@ -33,7 +32,6 @@ final class Version20220129170406 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE tirage ADD winner_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE tirage ADD CONSTRAINT FK_2A145AFF5DFCD4B8 FOREIGN KEY (winner_id) REFERENCES winner (id)');
         $this->addSql('CREATE INDEX IDX_2A145AFF5DFCD4B8 ON tirage (winner_id)');
         $this->addSql('ALTER TABLE winner DROP INDEX IDX_CF6600E82579054, ADD UNIQUE INDEX UNIQ_CF6600E82579054 (tirage_id)');
         $this->addSql('ALTER TABLE winner CHANGE tirage_id tirage_id INT NOT NULL');
